735787f8b5491f8a09ad32ab550425acfe7b300a
MarianP24 <p.marian24@yahoo.com>
2024-03-04 20:12:09 +0200

    last one

--- session4/src/com/itschool/course/CursMain4.java
+++ session4/src/com/itschool/course/CursMain4.java
@@ -3,5 +3,6 @@ package com.itschool.course;
 public class CursMain4 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello!");
     }
 }
